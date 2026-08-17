a2bbb7c799692417477079059eb88bf7f4eb3732
Josh May <Josh.May@monash.edu>
2026-08-17 11:15:08 +1000

    Exposed get_tc_age to be interfaced from utils

--- src/dav/utils/__init__.py
+++ src/dav/utils/__init__.py
@@ -1,6 +1,8 @@
-"""Make IbtracsReader and MergirReader readily available."""
+"""Utilities to assist with accessing data and extracting data useful for
+tropical cyclone characteristic estimations."""
 
 from .datareaders import IbtracsReader, MergirReader
 from .polar_feature_extractor import get_dav_profile
+from .tc_age import get_tc_age
 
-__all__ = ["IbtracsReader", "MergirReader", "get_dav_profile"]
+__all__ = ["IbtracsReader", "MergirReader", "get_dav_profile", "get_tc_age"]
