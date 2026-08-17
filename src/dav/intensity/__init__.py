ea7b6a9caed7bb4a454a74a502a05342742c7675
Josh May <Josh.May@monash.edu>
2026-08-17 11:12:28 +1000

    Removed public facing intensity module functions

--- src/dav/intensity/__init__.py
+++ src/dav/intensity/__init__.py
@@ -1,5 +1,1 @@
-"""Assits in interfacing with the intensity module."""
-from .model import predict as predict_intensity
-from .fitting import fit as fit_intensity
-
-__all__ = ["predict_intensity", "fit_intensity"]
+"""Interface with the intensity module."""
